e2353d1febe170f4a654f29134419ef49a5b1575
pvasquez07 <p.vasquez@live.ca>
2021-02-08 19:14:33 -0500

    Add flexibility by allowing for any iterable object to be parsed

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,62 +1,71 @@
+#!/usr/bin/env python3
 # fileparse.py
 #
 # Exercise 3.3
 
 import csv
+import sys
 
 
 def parse_csv(
-    filename,
+    lines,
     select=None,
     types=None,
-    has_headers=None,
+    has_headers=True,
     delimiter=",",
     silence_errors=False,
 ):
     """
     Parse a CSV file into a list of records with type conversion
+
+    lines: Any file-like or iterable object
+    select: option to specify columns as a list of strings
+    types: type conversion of record (e.g.,[str, int, float])
+    has_header: specify if file has header (bool)
+    delimiter: specify a particular delimiter
+    silence_errors: Option to not display errors (bool)
     """
+
     if select and not has_headers:
         raise RuntimeError("select requires column headers")
 
-    with open(filename) as f:
-        rows = csv.reader(f, delimiter=delimiter)
+    rows = csv.reader(lines, delimiter=delimiter)
 
-        # Read the file headers (if any)
-        headers = next(rows) if has_headers else []
+    # Read the file headers (if any)
+    headers = next(rows) if has_headers else []
 
-        # If a column selctor was given, find indices of the specified columns.
-        # Also narrow the set of headers used in the resulting dictionaires.
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
+    # If a column selctor was given, find indices of the specified columns.
+    # Also narrow the set of headers used in the resulting dictionaires.
+    if select:
+        indices = [headers.index(colname) for colname in select]
+        headers = select
+    else:
+        indices = []
 
-        records = []
-        for rowno, row in enumerate(rows, 1):
-            if not row:  # Skip rows with no data
+    records = []
+    for rowno, row in enumerate(rows, 1):
+        if not row:  # Skip rows with no data
+            continue
+
+        # Filter the row if specific columns were selected
+        if indices:
+            row = [row[index] for index in indices]
+
+        # Applies type conversion to the row
+        if types:
+            try:
+                row = [func(val) for func, val in zip(types, row)]
+            except ValueError as e:
+                if not silence_errors:
+                    print(f"Row {rowno}: Couldn't convert {row}")
+                    print(f"Row {rowno}: Reason {e}")
                 continue
 
-            # Filter the row if specific columns were selected
-            if indices:
-                row = [row[index] for index in indices]
-
-            # Applies type conversion to the row
-            if types:
-                try:
-                    row = [func(val) for func, val in zip(types, row)]
-                except ValueError as e:
-                    if not silence_errors:
-                        print(f"Row {rowno}: Couldn't convert {row}")
-                        print(f"Row {rowno}: Reason {e}")
-                    continue
-
-            # Make dictionary or tuple
-            if headers:
-                record = dict(zip(headers, row))
-            else:
-                record = tuple(row)
-            records.append(record)
+        # Make dictionary or tuple
+        if headers:
+            record = dict(zip(headers, row))
+        else:
+            record = tuple(row)
+        records.append(record)
 
     return records
