1d2717b02b855e2c11452d96016ad2eedbf06dc7
pvasquez07 <p.vasquez@live.ca>
2021-02-11 14:09:44 -0500

    initial commit

---
+++ Work/tableformat.py
@@ -0,0 +1,87 @@
+# tableformat.py
+
+
+class TableFormatter:
+    def headings(self, headers):
+        """
+        Emit the table headings.
+        """
+        raise NotImplementedError()
+
+    def row(self, rowdata):
+        """
+        Emit a single tow of table data.
+        """
+        raise NotImplementedError()
+
+
+class TextTableFormatter(TableFormatter):
+    """
+    Output a table in plain-text format
+    """
+
+    def headings(self, headers):
+        for h in headers:
+            print(f"{h:>10s}", end=" ")
+        print()
+        print(("-" * 10 + " ") * len(headers))
+
+    def row(self, rowdata):
+        for d in rowdata:
+            print(f"{d:>10s}", end=" ")
+        print()
+
+
+class CSVTableFormatter(TableFormatter):
+    """
+    Output a table in CSV format
+    """
+
+    def headings(self, headers):
+        print(",".join(headers))
+
+    def row(self, rowdata):
+        print(",".join(rowdata))
+
+
+class HTMLTableFormatter(TableFormatter):
+    """
+    Oupput a table in HTML format
+    """
+
+    def headings(self, headers):
+        print("<tr>", end="")
+        for h in headers:
+            print(f"<th>{h}</th>", end="")
+        print("</tr>")
+
+    def row(self, rowdata):
+        print("<tr>", end="")
+        for d in rowdata:
+            print(f"<td>{d}</td>", end="")
+        print("</tr>")
+
+
+class FormatError(Exception):
+    pass
+
+
+def create_formatter(name):
+    if name == "txt":
+        return TextTableFormatter()
+    elif name == "csv":
+        return CSVTableFormatter()
+    elif name == "html":
+        return HTMLTableFormatter()
+    else:
+        raise FormatError(f"Unkonwn table format {name}")
+
+
+def print_table(objects, columns, formatter):
+    """
+    Make a nice formatted table from a list of objects and attribute names.
+    """
+    formatter.headings(columns)
+    for obj in objects:
+        rowdata = [str(getattr(obj, name)) for name in columns]
+        formatter.row(rowdata)
